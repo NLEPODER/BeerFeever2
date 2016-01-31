2c78590f821180167cd7181cffa67d0543f53ece
NLEPODER <nicolas.lepoder@gmail.com>
2016-01-31 21:28:01 +0100

    Ajout dans chaque Scene du Tree / Controllers / et changeLevel.cs

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class changeScene : MonoBehaviour {
+
+    public void toMenu()
+    {
+        SceneManager.LoadScene("sMenu");
+    }
+    public void toGame()
+    {
+        SceneManager.LoadScene("sGame");
+    }
+    public void toOptions()
+    {
+        SceneManager.LoadScene("sOptions");
+    }
+}
