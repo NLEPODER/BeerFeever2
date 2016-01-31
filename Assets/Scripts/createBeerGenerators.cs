dad41945dbb7038cc09b932d1439a5b97942acec
ErwanLP <erwan.le-poder@isep.fr>
2016-01-31 22:49:25 +0100

    modif create beer Generator

--- Assets/Scripts/createBeerGenerators.cs
+++ Assets/Scripts/createBeerGenerators.cs
@@ -4,29 +4,27 @@ using System.Collections;
 public class createBeerGenerators : MonoBehaviour
 {
 
-    public static GameObject BeerGeneratorPrefab = Resources.Load("Prefabs/BeerGeneratorPrefab", typeof(GameObject)) as GameObject;
-
-    public static int speed;
-
+    private GameObject BeerGeneratorPrefab;
 
     // Use this for initialization
     void Start()
     {
-
+        BeerGeneratorPrefab = Resources.Load("Prefabs/BeerGeneratorPrefab", typeof(GameObject)) as GameObject;
         Vector3 posTop = new Vector3(0, 5.3f, 0);
         Vector3 posMid = new Vector3(0, -1.1f, 0);
         Vector3 posBottom = new Vector3(0, -8.5f, 0);
      
-        instantiateBeerGenerator(posTop);
-        instantiateBeerGenerator(posMid);
-        instantiateBeerGenerator(posBottom);
+        instantiateBeerGenerator(posTop, "brune");
+        instantiateBeerGenerator(posMid, "rousse");
+        instantiateBeerGenerator(posBottom, "blonde");
 
     }
 
-    void instantiateBeerGenerator(Vector3 pos)
+    void instantiateBeerGenerator(Vector3 pos, string beerGeneratorColor)
     {
         GameObject BeerGenerator;
-        BeerGenerator = Instantiate(createBeerGenerators.BeerGeneratorPrefab, pos, Quaternion.identity) as GameObject;
+        BeerGenerator = Instantiate(BeerGeneratorPrefab, pos, Quaternion.identity) as GameObject;
+        gameObject.GetComponent<beerGenerator_specific>().setBeerGeneratorColor(beerGeneratorColor);
     }
 
     // Update is called once per frame
