082aa4ebe16e8d5d6f04895fc7f64f5b09b20bc3
ErwanLP <erwan.le-poder@isep.fr>
2016-01-31 21:29:18 +0100

    beer spesific and creation of beer

---
+++ Assets/Scripts/beer_specific.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beer_specific : MonoBehaviour
+{
+
+    private string beerType;
+    private string beerColor;
+    private bool filled;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+         //transform.Translate(Game_controller.getSpeedBeer() * 1 * Time.deltaTime, 0, 0);
+    }
+
+    public void setBeerType(string beerType)
+    {
+        this.beerType = beerType;
+    }
+    public void setBeerColor(string beerColor)
+    {
+        this.beerColor = beerColor;
+    }
+
+}
--- Assets/Scripts/createBeer.cs
+++ Assets/Scripts/createBeer.cs
@@ -3,13 +3,43 @@ using System.Collections;
 
 public class createBeer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private GameObject beerPrefab;
+
+    // Use this for initialization
+    void Start () {
+        beerPrefab = Resources.Load("BeerPrefab", typeof(GameObject)) as GameObject;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        GameObject gameObject_beer =  Instantiate(beerPrefab, this.transform.position, Quaternion.identity) as GameObject;
+        string beerType = null;
+        Animator animator;
+        switch (Random.Range(0, 4))
+        {
+            case 1:
+                beerType = "demi";
+                animator = gameObject_beer.GetComponent<Animator>();
+                animator.SetBool("isDemi", true);
+                break;
+            case 2:
+                beerType = "pinte";
+                animator = gameObject_beer.GetComponent<Animator>();
+                animator.SetBool("isPinte", true);
+                break;
+            case 3:
+                beerType = "boot";
+                animator = gameObject_beer.GetComponent<Animator>();
+                animator.SetBool("isBoot", true);
+                break;
+        }
+        /**gameObject_beer.GetComponent<beer_specific>().beerType = beerType;
+        gameObject_beer.GetComponent<beer_specific>().beerColor = beerColor;
+        gameObject_beer.GetComponent<beer_specific>().filled = false;
+
+        gameObjectAllBeerList.Add(gameObject_beer);
+        }
+    */
+    }
 }
