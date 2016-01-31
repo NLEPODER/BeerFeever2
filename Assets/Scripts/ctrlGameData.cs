e642300b39574ffdfdd18db35448a001d17d3616
ErwanLP <erwan.le-poder@isep.fr>
2016-01-31 21:11:01 +0100

    beguin sscipt

---
+++ Assets/Scripts/ctrlGameData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ctrlGameData : MonoBehaviour {
+
+    private int score;
+    private float timer;
+    private float currentDifficulty;
+    private float increaseDifficultyRatio;
+    private float increaseDifficultyTime;
+    private float initTime;
+    private float currentTime;
+    private Vector3 center;
+
+
+    // Use this for initialization
+    void Start () {
+        score = 0;
+        currentDifficulty = 1;
+        increaseDifficultyRatio = 1.1f;
+        increaseDifficultyTime = 2;
+        initTime = Time.time;
+        center = new Vector3(0, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime = Time.time;
+	
+	}
+}
