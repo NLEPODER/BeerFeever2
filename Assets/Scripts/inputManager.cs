e651c46670fd0004aee0a7f53fafebbdb9a99d33
ErwanLP <erwan.le-poder@isep.fr>
2016-02-01 23:23:55 +0100

    test foreach

--- Assets/Scripts/inputManager.cs
+++ Assets/Scripts/inputManager.cs
@@ -5,6 +5,11 @@ public class inputManager : MonoBehaviour {
 
     public void clickButtonTypeBeer(string beerType)
     {
+        GameObject[] beerList = GameObject.FindGameObjectsWithTag("BeerPrefab");
+        foreach (GameObject beer in beerList)
+        {
+            Debug.Log("test");
+        }
         Debug.Log(beerType);
     }
 }
