5471dee6c8536306d0c1448b9be4fda423343ed7
ErwanLP <erwan.le-poder@isep.fr>
2016-02-01 23:11:15 +0100

    add button

---
+++ Assets/Scripts/inputManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inputManager : MonoBehaviour {
+
+    public void clickButtonTypeBeer(string beerType)
+    {
+        Debug.Log(beerType);
+    }
+}
