0494d83f526cc2151a65f80bd770dfef1bc9e66d
ErwanLP <erwan.le-poder@isep.fr>
2016-02-01 23:29:31 +0100

    add Tag

--- Assets/Scripts/inputManager.cs
+++ Assets/Scripts/inputManager.cs
@@ -5,7 +5,7 @@ public class inputManager : MonoBehaviour {
 
     public void clickButtonTypeBeer(string beerType)
     {
-        GameObject[] beerList = GameObject.FindGameObjectsWithTag("BeerPrefab");
+        GameObject[] beerList = GameObject.FindGameObjectsWithTag("Tag_Beer");
         foreach (GameObject beer in beerList)
         {
             Debug.Log("test");
